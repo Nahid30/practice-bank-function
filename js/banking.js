d59ef49cb4d8a2dd371a1e80742a7c2b9e498db4
Nahid30 <nahidshuvo30@gmail.com>
2022-02-15 23:20:04 +0600

    done by function also not deleted raw code

--- js/banking.js
+++ js/banking.js
@@ -1,27 +1,50 @@
 
 
+function getInputValue(inputId){
+    const inputField = document.getElementById(inputId);
+    const inputFieldValue = inputField.value;
+    const inputFieldValueParse = parseFloat(inputFieldValue);
+    inputField.value = '';
+    return inputFieldValueParse;
+}
+
+
+function updateTotalField(totalFieldId,amountValue){
+
+    const totalElements = document.getElementById(totalFieldId);
+    const totalInnerText = totalElements.innerText;
+    const totalInnerTextParse = parseFloat(totalInnerText);
+
+    const totalAmount = totalInnerTextParse + amountValue   ;
+    totalElements.innerText = totalAmount;
+
+
+}
+
+
 
 
 document.getElementById('deposit-button').addEventListener('click',function(){
    
 
-    const depositInputField = document.getElementById('deposit-input');
+    /* const depositInputField = document.getElementById('deposit-input');
     const depositInputFieldValue = depositInputField.value;
-    const depositInputFieldValueParse = parseFloat(depositInputFieldValue); 
+    const depositInputFieldValueParse = parseFloat(depositInputFieldValue);  */
 
-    depositInputField.value = '';
+    const depositInputFieldValueParse = getInputValue('deposit-input');
 
     
     
-    const depositTotal = document.getElementById('deposit-total');
+   /*  const depositTotal = document.getElementById('deposit-total');
     const depositTotalInnerText = depositTotal.innerText;
     const depositTotalInnerTextParse = parseFloat(depositTotalInnerText);
     
 
-    const depositTotalAmount = depositInputFieldValueParse + depositTotalInnerTextParse ;
+    const depositTotalAmount = depositInputFieldValueParse + depositTotalInnerTextParse ; */
 
-    depositTotal.innerText = depositTotalAmount;
+    updateTotalField('deposit-total',depositInputFieldValueParse);
     
+
 
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalInnerText = balanceTotal.innerText;
@@ -37,20 +60,23 @@ document.getElementById('deposit-button').addEventListener('click',function(){
 
 document.getElementById('withdraw-button').addEventListener('click',function(){
 
-    const withdrawInputField = document.getElementById('withdraw-input');
+    /* const withdrawInputField = document.getElementById('withdraw-input');
     const withdrawInputFieldValue = withdrawInputField.value;
-    const withdrawInputFieldValueParse = parseFloat(withdrawInputFieldValue);
+    const withdrawInputFieldValueParse = parseFloat(withdrawInputFieldValue); */
+
+    const withdrawInputFieldValueParse = getInputValue('withdraw-input')
 
 
-    const withdrawTotal = document.getElementById('withdraw-total');
+   /*  const withdrawTotal = document.getElementById('withdraw-total');
     const withdrawTotalInnerText = withdrawTotal.innerText;
     const withdrawTotalInnerTextParse = parseFloat(withdrawTotalInnerText);
 
+    const withdrawTotalAmount = withdrawTotalInnerTextParse + withdrawInputFieldValueParse;
+    withdrawTotal.innerText = withdrawTotalAmount; */
 
-    const withdrawTotalAmount = withdrawInputFieldValueParse + withdrawTotalInnerTextParse;
-    withdrawTotal.innerText = withdrawTotalAmount;
+    updateTotalField ('withdraw-total', withdrawInputFieldValueParse);
 
-    withdrawInputField.value = '';
+
 
     
     const balanceTotalNew = document.getElementById('balance-total');
